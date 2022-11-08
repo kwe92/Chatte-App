04038d3abe049582619c6ec80abcd0a0f2857a61
Kweayon  Clark <Kweayon@gmail.com>
2022-11-08 17:28:24 -0600

    Layout File Added

---
+++ lib/src/constants/source_of_truth.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  const AppColor();
+  static final main = Colors.deepPurple[300];
+  static const appBar = Colors.deepPurple;
+  static final backGround = Colors.deepPurple[100];
+  // static final main = Colors.deepPurple[200];
+  // static final main = Colors.deepPurple[200];
+}
