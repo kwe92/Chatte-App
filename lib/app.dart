d9884c8fe2768a0a7ac56e4d05cf6f5a2f69e691
Kweayon  Clark <Kweayon@gmail.com>
2022-11-21 15:51:14 -0600

    Added Navigator

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,9 @@
 import 'package:chatapp/src/constants/source_of_truth.dart';
+import 'package:chatapp/src/features/authentication/presentation/auth_form.dart';
+import 'package:chatapp/src/features/authentication/presentation/auth_screen.dart';
 import 'package:chatapp/src/features/chat/presentation/chat_screen.dart';
+import 'package:chatapp/src/features/create_user/create_form.dart';
+import 'package:chatapp/src/features/create_user/create_user_screem.dart';
 import 'package:flutter/material.dart';
 
 const _title = 'Flutter CHat';
@@ -12,7 +16,16 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData(primarySwatch: AppColor.appBar),
       title: _title,
-      home: const ChatScreen(title: _title),
+      initialRoute: '/',
+      routes: <String, WidgetBuilder>{
+        '/': (context) => AuthScreen(body: AuthForm(
+              onPressed: () {
+                Navigator.pushNamed(context, '/createuser');
+              },
+            )),
+        '/chatscreen': (context) => const ChatScreen(title: _title),
+        '/createuser': (context) => const CreateScreen(body: CreateForm()),
+      },
     );
   }
 }
