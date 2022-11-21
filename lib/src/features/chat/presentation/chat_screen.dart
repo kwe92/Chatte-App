879f50585bb1d47b7e049909b16a90bf98e32e40
Kweayon  Clark <Kweayon@gmail.com>
2022-11-21 15:52:55 -0600

    Updates

--- lib/src/features/chat/presentation/chat_screen.dart
+++ lib/src/features/chat/presentation/chat_screen.dart
@@ -1,7 +1,5 @@
-import 'dart:math';
-
 import 'package:chatapp/src/constants/source_of_truth.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:chatapp/src/utils/stream_firestore.dart';
 import 'package:flutter/material.dart';
 
 class ChatScreen extends StatelessWidget {
@@ -9,37 +7,48 @@ class ChatScreen extends StatelessWidget {
   final String title;
   @override
   Widget build(BuildContext context) {
-    var _i = 0;
     return SafeArea(
       child: Scaffold(
-        backgroundColor: AppColor.backGround,
         appBar: AppBar(
-          title: Text(title),
+          title: const Text('Firebase Demo'),
         ),
-        body: ListView.builder(
-          itemCount: 8,
-          itemBuilder: (context, index) => Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              height: 110,
-              decoration: BoxDecoration(color: AppColor.main),
-              child: Center(child: Text('Dummy text ${_i++}')),
-            ),
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          backgroundColor: Colors.deepPurple[200],
-          onPressed: () {
-            // FirebaseFirestore.instance
-            //     .collection('chats/amAflxUTjTvrI261RJYi/messages')
-            //     .snapshots()
-            //     .listen((event) {
-            //   print(event);
-            // });
-          },
-          child: const Icon(
-            Icons.add,
-          ),
+        body: StreamBuilder(
+          stream: StreamFireStore.getListDocsData(
+              collectionPath: 'chats/amAflxUTjTvrI261RJYi/messages'),
+          builder: ((context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const CircularProgressIndicator.adaptive();
+            }
+            if (snapshot.hasError) {
+              return Center(
+                child: Text(
+                  snapshot.error.toString(),
+                ),
+              );
+            }
+            final List<Map<String, dynamic>> msgs = snapshot.data!;
+            return ListView.builder(
+              // padding: EdgeInsets.all(10),
+              itemCount: snapshot.data?.length,
+              itemBuilder: ((context, index) {
+                debugPrint(snapshot.data.toString());
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    height: 110,
+                    decoration: BoxDecoration(color: AppColor.main),
+                    child: Center(
+                      child: Text(
+                        msgs[index]['text'].toString(),
+                        style: const TextStyle(
+                            fontSize: 18.0, color: Colors.white),
+                      ),
+                    ),
+                  ),
+                );
+              }),
+            );
+          }),
         ),
       ),
     );
