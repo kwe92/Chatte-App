0c71f8f443e440734775b0320046d71668b31701
Kweayon  Clark <Kweayon@gmail.com>
2022-11-21 15:51:49 -0600

    Updates

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,3 @@
-import 'package:chatapp/src/constants/source_of_truth.dart';
-import 'package:chatapp/src/utils/stream_firestore.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 
@@ -10,63 +7,6 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(
-      //const MyApp(),
-      const MaterialApp(
-    home: FireBaseDemo(),
-  ));
-}
-
-// Stream<List> watchMessages() => FirebaseFirestore.instance
-//     .collection('chats/amAflxUTjTvrI261RJYi/messages')
-//     .snapshots()
-//     .map((snapshot) => snapshot.docs.map((doc) => doc.data()).toList());
-
-// Stream<QuerySnapshot<Map<String, dynamic>>> returnCollectionSnapshot(
-//         {required String collectionPath}) =>
-//     FirebaseFirestore.instance.collection(collectionPath).snapshots();
-
-// Stream<List<Map<String, dynamic>>> returnListDocsData(
-//         {required Stream<QuerySnapshot<Map<String, dynamic>>> snapshot}) =>
-//     snapshot.map((snapshot) => snapshot.docs.map((doc) => doc.data()).toList());
-
-// final testStream = returnListDocsData(
-//     snapshot: returnCollectionSnapshot(
-//         collectionPath: 'chats/amAflxUTjTvrI261RJYi/messages'));
-
-class FireBaseDemo extends StatelessWidget {
-  const FireBaseDemo({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text('Firebase Demo'),
-        ),
-        body: StreamBuilder(
-          stream: StreamFireStore.getListDocsData(
-              collectionPath: 'chats/amAflxUTjTvrI261RJYi/messages'),
-          builder: ((context, snapshot) {
-            if (!snapshot.hasData) {
-              return const SizedBox.shrink();
-            }
-            return ListView.builder(
-              itemCount: snapshot.data?.length,
-              itemBuilder: ((context, index) {
-                print(snapshot.data);
-                return Container(
-                  height: 110,
-                  decoration: BoxDecoration(color: AppColor.main),
-                  child: Center(
-                    child: Text(
-                      snapshot.data![0]['text'].toString(),
-                    ),
-                  ),
-                );
-              }),
-            );
-          }),
-        ),
-      ),
-    );
-  }
+    const MyApp(),
+  );
 }
--- lib/src/utils/stream_firestore.dart
+++ lib/src/utils/stream_firestore.dart
@@ -3,42 +3,55 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class StreamFireStore {
   const StreamFireStore();
 
-  /// Takes a collection path to the currently configured [FirebaseFirestore]
+  /// [FirebaseFirestore] instance
+  static FirebaseFirestore get _firebaseFirestore => FirebaseFirestore.instance;
+
+  /// [CollectionReference] to the specified Firestore collection path
+  static CollectionReference<Map<String, dynamic>> _returnCollection(
+          {required FirebaseFirestore firestore,
+          required String collectionPath}) =>
+      firestore.collection(collectionPath);
+
+  /// Takes a [CollectionReference] to the currently configured [FirebaseFirestore]
   /// Returns a [QuerySnapshot]
-  /// The returned [QuerySnapshot] contains the results of a query.
-  /// It can contain zero or more [DocumentSnapshot] objects,
+  /// The returned [QuerySnapshot] contains the results of a query
+  /// It can contain zero or more [DocumentSnapshot] objects
 
   static Stream<QuerySnapshot<Map<String, dynamic>>> _returnCollectionSnapshot(
-          {required String collectionPath}) =>
-      FirebaseFirestore.instance.collection(collectionPath).snapshots();
+          {required CollectionReference<Map<String, dynamic>> collectionRef}) =>
+      collectionRef.snapshots();
 
   /// Takes a [QuerySnapshot] as an argument
-  /// Returns a [List] of all [QueryDocumentSnapshot] objects.
+  /// Returns a [List] of all [QueryDocumentSnapshot] objects
   /// A [QueryDocumentSnapshot] object
   /// contains data read from a document in your [FirebaseFirestore]
-  /// database as part of a query.
+  /// database as part of a query
 
   static Stream<List<QueryDocumentSnapshot<Map<String, dynamic>>>>
       _returnListDocsSnapshot(
               {required Stream<QuerySnapshot<Map<String, dynamic>>>
                   snapshot}) =>
-          snapshot.map((snapshot) => snapshot.docs.map((doc) => doc).toList());
+          snapshot.map((snapshot) => snapshot.docs);
 
-  /// Takes a [List] of [QueryDocumentSnapshot] objects as a argument.
+  /// Takes a [List] of [QueryDocumentSnapshot] objects as a argument
   /// Returns a [List] of each [QueryDocumentSnapshot] objects data as a [Map]
 
   static Stream<List<Map<String, dynamic>>> _returnListDocsData(
           {required Stream<List<QueryDocumentSnapshot<Map<String, dynamic>>>>
               documentSnapshots}) =>
-      documentSnapshots
-          .map((snapshot) => snapshot.map((doc) => doc.data()).toList());
+      documentSnapshots.map((listDocSnapshot) =>
+          listDocSnapshot.map((doc) => doc.data()).toList());
 
   /// Returns a [List] of [Map]<String,dynamic> to the user
   static Stream<List<Map<String, dynamic>>> getListDocsData(
           {required String collectionPath}) =>
       StreamFireStore._returnListDocsData(
         documentSnapshots: _returnListDocsSnapshot(
-          snapshot: _returnCollectionSnapshot(collectionPath: collectionPath),
+          snapshot: _returnCollectionSnapshot(
+            collectionRef: _returnCollection(
+                firestore: StreamFireStore._firebaseFirestore,
+                collectionPath: collectionPath),
+          ),
         ),
       );
 }
