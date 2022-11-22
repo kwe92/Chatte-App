0b2515c1fa5bce7e3df2fefae820cad8b53199f5
Kweayon  Clark <Kweayon@gmail.com>
2022-11-22 13:27:15 -0600

    Update & Refactor

--- lib/src/features/create_user/presentation/create_form.dart
+++ lib/src/features/create_user/presentation/create_form.dart
@@ -1,4 +1,9 @@
 import 'package:chatapp/src/constants/source_of_truth.dart';
+import 'package:chatapp/src/features/create_user/domain/user_model.dart';
+import 'package:chatapp/src/utils/validator.dart';
+import 'package:chatapp/src/widgets/form_fields.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class CreateForm extends StatefulWidget {
@@ -8,48 +13,74 @@ class CreateForm extends StatefulWidget {
   State<CreateForm> createState() => _CreateFormState();
 }
 
+Future<void> _createUser(
+    {required UserModel user,
+    required DocumentReference<Map<String, dynamic>> docRef}) async {
+  final json = user.toJSON();
+  await docRef.set(json);
+  await FirebaseAuth.instance.createUserWithEmailAndPassword(
+      email: user.email, password: user.password);
+}
+
 class _CreateFormState extends State<CreateForm> {
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     TextEditingController emailController = TextEditingController();
     TextEditingController passwordController = TextEditingController();
     TextEditingController userNameController = TextEditingController();
+
     return Center(
+      //TODO: Move the card into its own module
       child: Card(
         margin: const EdgeInsets.all(12.0),
         child: SingleChildScrollView(
           child: Padding(
             padding: const EdgeInsets.all(12.0),
             child: Form(
+              key: _formKey,
               child: Column(
                 mainAxisSize: MainAxisSize.min,
                 children: <Widget>[
-                  TextFormField(
-                    controller: emailController,
-                    keyboardType: TextInputType.emailAddress,
-                    decoration: const InputDecoration(labelText: 'e-mail'),
-                  ),
-                  TextFormField(
-                    controller: userNameController,
-                    keyboardType: TextInputType.text,
-                    decoration: const InputDecoration(labelText: 'username'),
-                  ),
-                  TextFormField(
-                    controller: passwordController,
-                    obscureText: true,
-                    keyboardType: TextInputType.text,
-                    decoration: const InputDecoration(labelText: 'password'),
+                  FormFields(
+                    userNameController: userNameController,
+                    passwordController: passwordController,
+                    emailController: emailController,
+                    isLogin: true,
                   ),
                   gaph16,
                   SizedBox(
                     width: 400,
                     child: ElevatedButton(
-                      onPressed: () {
-                        Navigator.pop(context);
+                      onPressed: () async {
+                        final valid = Validator.trySubmit(_formKey);
+                        if (valid) {
+                          final docRef = FirebaseFirestore.instance
+                              .collection('users')
+                              .doc();
+                          final UserModel usr = UserModel(
+                              id: docRef.id,
+                              userName: userNameController.text,
+                              password: passwordController.text,
+                              email: emailController.text);
+                          await _createUser(user: usr, docRef: docRef);
+                          // ignore: use_build_context_synchronously
+                          Navigator.pushReplacementNamed(context, '/');
+                        } else {
+                          // ignore: todo
+                          //TODO: Inform the user why the button did nothing on press
+                          return;
+                        }
                       },
                       child: const Text('Submit'),
                     ),
                   ),
+                  TextButton(
+                      onPressed: () {
+                        Navigator.pushReplacementNamed(context, '/');
+                      },
+                      child: const Text('I already have an account'))
                 ],
               ),
             ),
--- lib/src/features/create_user/presentation/create_user_screen.dart
+++ lib/src/features/create_user/presentation/create_user_screen.dart
@@ -4,8 +4,7 @@
 //    - AuthForm
 // nl
 
-import 'package:chatapp/src/constants/source_of_truth.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:chatapp/src/widgets/field_scaffold.dart';
 import 'package:flutter/material.dart';
 
 class CreateScreen extends StatefulWidget {
@@ -17,24 +16,13 @@ class CreateScreen extends StatefulWidget {
   State<CreateScreen> createState() => _CreateScreenState();
 }
 
-Future<void> _createUser(
-    {required name, required age, required birthdate}) async {
-  final docUsers = FirebaseFirestore.instance.collection('users').doc();
-  // final newUser =
-  //     UserModel(id: docUsers.id, name: name, age: age, birthdate: birthdate);
-  // final json = newUser.toJSON();
-  // await docUsers.set(json);
-}
-
 class _CreateScreenState extends State<CreateScreen> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(title: const Text('Flutter Chat: Sign Up')),
-        backgroundColor: Theme.of(context).backgroundColor,
-        body: widget.body,
-      ),
+    return FieldScaffold(
+      body: widget.body,
+      title: 'Flutter Chat: Sign Up',
+      bgColor: Theme.of(context).backgroundColor,
     );
   }
 }
