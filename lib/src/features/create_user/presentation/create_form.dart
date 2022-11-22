da7bb844f6351ddbdae9f2a12c032755620fac95
Kweayon  Clark <Kweayon@gmail.com>
2022-11-22 16:55:55 -0600

    Handled Existing User

--- lib/src/features/create_user/presentation/create_form.dart
+++ lib/src/features/create_user/presentation/create_form.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_final_fields
+
 import 'package:chatapp/src/constants/source_of_truth.dart';
 import 'package:chatapp/src/features/create_user/domain/user_model.dart';
 import 'package:chatapp/src/utils/validator.dart';
@@ -14,17 +16,27 @@ class CreateForm extends StatefulWidget {
 }
 
 Future<void> _createUser(
-    {required UserModel user,
-    required DocumentReference<Map<String, dynamic>> docRef}) async {
+    {required TextEditingController userNameController,
+    required TextEditingController passwordController,
+    required TextEditingController emailController,
+    required CollectionReference<Map<String, dynamic>> docRef}) async {
+  final userCredentials = await FirebaseAuth.instance
+      .createUserWithEmailAndPassword(
+          email: emailController.text, password: passwordController.text);
+  final UserModel user = UserModel(
+      id: userCredentials.user!.uid,
+      userName: userNameController.text,
+      password: passwordController.text,
+      email: emailController.text);
   final json = user.toJSON();
-  await docRef.set(json);
-  await FirebaseAuth.instance.createUserWithEmailAndPassword(
-      email: user.email, password: user.password);
+  await docRef.doc(user.id).set(json);
 }
 
 class _CreateFormState extends State<CreateForm> {
   final _formKey = GlobalKey<FormState>();
-
+  bool _isLoading = false;
+  bool _userExists = false;
+  String _errMsg = '';
   @override
   Widget build(BuildContext context) {
     TextEditingController emailController = TextEditingController();
@@ -33,60 +45,76 @@ class _CreateFormState extends State<CreateForm> {
 
     return Center(
       //TODO: Move the card into its own module
-      child: Card(
-        margin: const EdgeInsets.all(12.0),
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(12.0),
-            child: Form(
-              key: _formKey,
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  FormFields(
-                    userNameController: userNameController,
-                    passwordController: passwordController,
-                    emailController: emailController,
-                    isLogin: true,
-                  ),
-                  gaph16,
-                  SizedBox(
-                    width: 400,
-                    child: ElevatedButton(
-                      onPressed: () async {
-                        final valid = Validator.trySubmit(_formKey);
-                        if (valid) {
-                          final docRef = FirebaseFirestore.instance
-                              .collection('users')
-                              .doc();
-                          final UserModel usr = UserModel(
-                              id: docRef.id,
-                              userName: userNameController.text,
-                              password: passwordController.text,
-                              email: emailController.text);
-                          await _createUser(user: usr, docRef: docRef);
-                          // ignore: use_build_context_synchronously
-                          Navigator.pushReplacementNamed(context, '/');
-                        } else {
-                          // ignore: todo
-                          //TODO: Inform the user why the button did nothing on press
-                          return;
-                        }
-                      },
-                      child: const Text('Submit'),
+      child: _isLoading
+          ? CircularProgressIndicator.adaptive()
+          : Card(
+              margin: const EdgeInsets.all(12.0),
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Form(
+                    key: _formKey,
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        _userExists
+                            ? Text(
+                                _errMsg,
+                                style: TextStyle(color: Colors.red),
+                              )
+                            : const SizedBox(),
+                        FormFields(
+                          userNameController: userNameController,
+                          passwordController: passwordController,
+                          emailController: emailController,
+                          isLogin: true,
+                        ),
+                        gaph16,
+                        SizedBox(
+                          width: 400,
+                          child: ElevatedButton(
+                            onPressed: () async {
+                              final valid = Validator.trySubmit(_formKey);
+                              if (valid) {
+                                final docRef = FirebaseFirestore.instance
+                                    .collection('users');
+                                try {
+                                  setState(() {
+                                    _isLoading = true;
+                                  });
+                                  await _createUser(
+                                      userNameController: userNameController,
+                                      passwordController: passwordController,
+                                      emailController: emailController,
+                                      docRef: docRef);
+                                  // ignore: use_build_context_synchronously
+                                  Navigator.pushReplacementNamed(context, '/');
+                                } catch (e) {
+                                  setState(() {
+                                    _userExists = true;
+                                    _errMsg = e.toString();
+                                    _isLoading = false;
+                                  });
+                                  debugPrint(e.toString());
+                                }
+                              } else {
+                                return;
+                              }
+                            },
+                            child: const Text('Submit'),
+                          ),
+                        ),
+                        TextButton(
+                            onPressed: () {
+                              Navigator.pushReplacementNamed(context, '/');
+                            },
+                            child: const Text('I already have an account'))
+                      ],
                     ),
                   ),
-                  TextButton(
-                      onPressed: () {
-                        Navigator.pushReplacementNamed(context, '/');
-                      },
-                      child: const Text('I already have an account'))
-                ],
+                ),
               ),
             ),
-          ),
-        ),
-      ),
     );
   }
 }
