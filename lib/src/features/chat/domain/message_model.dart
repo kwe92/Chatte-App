dab9dc886b7d6441f6c25a1027e4e88c86d4a676
Kweayon  Clark <Kweayon@gmail.com>
2022-11-23 12:35:06 -0600

    Added MessageModel

---
+++ lib/src/features/chat/domain/message_model.dart
@@ -0,0 +1,39 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class MessageModel {
+  const MessageModel(
+      {required this.userID, required this.textID, required this.text});
+  final String userID;
+  final String textID;
+  final String text;
+  Timestamp get _timeStamp => Timestamp.now();
+
+  Map<String, dynamic> toJSON() {
+    return {
+      'userid': userID,
+      'textid': textID,
+      'text': text.trim(),
+      'timestamp': _timeStamp,
+    };
+  }
+
+  factory MessageModel.fromJSON(Map<String, dynamic> json) {
+    return MessageModel(
+      userID: json['userid'],
+      textID: json['textid'],
+      text: json['text'],
+    );
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is MessageModel &&
+        other.textID == textID &&
+        other.text == text;
+  }
+
+  @override
+  int get hashCode => textID.hashCode ^ text.hashCode;
+}
