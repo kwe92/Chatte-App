10d2bb3c6daaf677bcb2db2f36eb61d3155ab798
Kweayon  Clark <Kweayon@gmail.com>
2022-11-24 09:36:35 -0600

    Refactored
    Added textID and userID

--- lib/src/features/chat/domain/message_model.dart
+++ lib/src/features/chat/domain/message_model.dart
@@ -3,14 +3,19 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 //TODO: Add UserName to the MessageModel to display to the chatscreen
 class MessageModel {
   const MessageModel(
-      {required this.userID, required this.textID, required this.text});
+      {required this.userID,
+      required this.userName,
+      required this.textID,
+      required this.text});
   final String userID;
+  final String userName;
   final String textID;
   final String text;
   Timestamp get _timeStamp => Timestamp.now();
 
   Map<String, dynamic> toJSON() {
     return {
+      'username': userName,
       'userid': userID,
       'textid': textID,
       'text': text.trim(),
@@ -20,6 +25,7 @@ class MessageModel {
 
   factory MessageModel.fromJSON(Map<String, dynamic> json) {
     return MessageModel(
+      userName: json['username'],
       userID: json['userid'],
       textID: json['textid'],
       text: json['text'],
@@ -31,14 +37,22 @@ class MessageModel {
     if (identical(this, other)) return true;
 
     return other is MessageModel &&
+        other.userID == userID &&
+        other.userName == userName &&
         other.textID == textID &&
         other.text == text;
   }
 
   @override
-  int get hashCode => textID.hashCode ^ text.hashCode;
+  int get hashCode {
+    return userID.hashCode ^
+        userName.hashCode ^
+        textID.hashCode ^
+        text.hashCode;
+  }
 
   @override
-  String toString() =>
-      'MessageModel(userID: $userID, textID: $textID, text: $text)';
+  String toString() {
+    return 'MessageModel(userID: $userID, userName: $userName, textID: $textID, text: $text)';
+  }
 }
