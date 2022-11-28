d114bd31e087ebec624fe2a8e86116659ad25f59
Kweayon  Clark <Kweayon@gmail.com>
2022-11-27 23:47:37 -0600

    Refactor
    Added the ability to show user picture and the UI changes depending on which user is logged in

--- lib/src/features/chat/presentation/chat_bubble.dart
+++ lib/src/features/chat/presentation/chat_bubble.dart
@@ -1,6 +1,11 @@
+import 'dart:io';
+
+import 'package:chatapp/src/constants/source_of_truth.dart';
 import 'package:chatapp/src/features/chat/domain/message_model.dart';
 import 'package:chatapp/src/features/create_user/domain/user_model.dart';
 import 'package:flutter/material.dart';
+
+//TODO: Save image URL to the text?? need to be able to see other users images
 
 //TODO: TextTile bubble? with a container?
 //TODO: Change the color and positioning of the buble depending on who is logged in
@@ -26,25 +31,41 @@ class ChatBubble extends StatelessWidget {
           ? MainAxisAlignment.end
           : MainAxisAlignment.start,
       children: [
-        Container(
-          padding: const EdgeInsets.all(16),
-          width: 160,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(30),
-            color: currentUser.id == message.userid
-                ? Colors.grey[500]
-                : Theme.of(context).primaryColor,
-          ),
-          child: Column(
-            children: [
-              Text(message.username, style: _style(fontSize: 16)),
-              Text(
-                message.text,
-                style: _style(weight: null),
+        currentUser.id != message.userid
+            ? CircleAvatar(
+                radius: 30,
+                backgroundImage: NetworkImage(message.userImageUrl),
               )
-            ],
+            : const SizedBox(),
+        gapw8,
+        Expanded(
+          child: Container(
+            padding: const EdgeInsets.all(24),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(30),
+              color: currentUser.id == message.userid
+                  ? Colors.grey[500]
+                  : Theme.of(context).primaryColor,
+            ),
+            child: Column(
+              children: [
+                Text(message.username, style: _style(fontSize: 16)),
+                gaph4,
+                Text(
+                  message.text,
+                  style: _style(weight: null),
+                )
+              ],
+            ),
           ),
         ),
+        currentUser.id == message.userid ? gapw8 : const SizedBox(),
+        currentUser.id == message.userid
+            ? CircleAvatar(
+                radius: 30,
+                backgroundImage: NetworkImage(message.userImageUrl),
+              )
+            : const SizedBox(),
       ],
     );
   }
