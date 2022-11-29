8a90d96c590417cdad35e4706dffd218bbb42e81
Kweayon  Clark <Kweayon@gmail.com>
2022-11-29 11:15:48 -0600

    Additional Implementations

--- lib/src/utils/user_options.dart
+++ lib/src/utils/user_options.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:chatapp/src/features/chat/domain/message_model.dart';
 import 'package:chatapp/src/features/create_user/domain/user_model.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -9,6 +10,7 @@ import 'package:flutter/material.dart';
 class UserOptions {
   const UserOptions();
 
+  // Create a User
   static Future<void> createUser(
       {required TextEditingController userNameController,
       required TextEditingController passwordController,
@@ -35,7 +37,28 @@ class UserOptions {
     await colRef.doc(user.id).set(json);
   }
 
+// Retrieve a single user by id
   static Future<DocumentSnapshot<Map<String, dynamic>>> getUser(
           {required collection, required userid}) async =>
       await FirebaseFirestore.instance.collection(collection).doc(userid).get();
+
+// Delete a message given the currently logged in user
+  static Future<void> deleteMessage(
+      {required String id, required String path}) async {
+    final docUsers = FirebaseFirestore.instance.collection(path).doc(id);
+    await docUsers.delete();
+  }
+
+  // Send a message by current user
+  static void sendMessage(UserModel user, String text, String path) {
+    final colRef = FirebaseFirestore.instance.collection(path);
+    final textID = colRef.doc().id;
+    MessageModel newMessage = MessageModel(
+        userid: user.id,
+        username: user.username,
+        userImageUrl: user.url,
+        textID: textID,
+        text: text);
+    colRef.doc(textID).set(newMessage.toJSON());
+  }
 }
