a869b4623d6666922aaa3a961436fc80288ab1b7
Kwe <Kweayon@gmail.com>
2024-01-28 21:02:43 -0600

    implemented abstract class BaseUser

---
+++ lib/shared/models/base_user.dart
@@ -0,0 +1,16 @@
+abstract class BaseUser {
+  const BaseUser({
+    required this.id,
+    required this.email,
+    required this.password,
+    required this.username,
+    required this.url,
+  });
+  final String id;
+  final String password;
+  final String email;
+  final String username;
+  final String url;
+
+  Map<String, dynamic> toJSON();
+}
