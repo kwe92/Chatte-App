d9a3770b8395aa0555d41b633a89fbf9c1d54e48
Kwe <Kweayon@gmail.com>
2024-01-29 23:12:56 -0600

    feat: added ChatService and UserService

--- lib/shared/services/services.dart
+++ lib/shared/services/services.dart
@@ -1,6 +1,13 @@
-import 'package:chatapp/shared/services/firebase_auth_service.dart';
+import 'package:chatapp/app/navigation/app_navigator.dart';
+import 'package:chatapp/shared/services/chat_service.dart';
+import 'package:chatapp/shared/services/firebase_service.dart';
 import 'package:chatapp/shared/services/firestore_service.dart';
 import 'package:chatapp/shared/services/get_it.dart';
+import 'package:chatapp/shared/services/user_service.dart';
 
 FireStoreService get firestoreService => locator.get<FireStoreService>();
-FirebaseAuthService get firebaseAuthService => locator.get<FirebaseAuthService>();
+FirebaseService get firebaseService => locator.get<FirebaseService>();
+AppNavigator get appNavigator => locator.get<AppNavigator>();
+UserService get userService => locator.get<UserService>();
+
+ChatService get chatService => locator.get<ChatService>();
