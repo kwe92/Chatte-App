38c5e0e27acaff739e9a0ab81dd06a736167b660
Kwe <Kweayon@gmail.com>
2024-01-31 14:39:41 -0600

    refact: changed the names of colors

--- lib/app/theme/colors.dart
+++ lib/app/theme/colors.dart
@@ -4,6 +4,16 @@ class AppColor {
   const AppColor._();
   static const grey0 = Color(0xffd3d3d3);
 
+  static const grey1 = Color(0xffc5c5c5);
+
+  static const grey2 = Color(0xff938886);
+
+  static const grey3 = Color(0xff858585);
+
+  static const black0 = Color(0xff262626);
+
+  static const offWhite = Color(0xffe1e1e1);
+
   static const primaryPurple = Color(0xff6200ee);
 
   static const primaryBlue = Color(0xff37a1ff);
