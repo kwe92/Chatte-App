afa39375dd8d4ecd7dfb45ed3454361121cd1367
Kwe <Kweayon@gmail.com>
2024-01-31 14:39:23 -0600

    feat: extended the vertical and horizontal gap properties

--- lib/app/resources/reusables.dart
+++ lib/app/resources/reusables.dart
@@ -1,37 +1,45 @@
 import 'package:flutter/material.dart';
 
-const gaph4 = SizedBox(
-  height: 4.0,
-);
+// Vertical Gaps
+const gapH4 = SizedBox(height: 4.0);
+const gapH8 = SizedBox(height: 8.0);
+const gapH12 = SizedBox(height: 12.0);
+const gapH16 = SizedBox(height: 16.0);
+const gapH18 = SizedBox(height: 18.0);
+const gapH20 = SizedBox(height: 20.0);
+const gapH24 = SizedBox(height: 24.0);
+const gapH26 = SizedBox(height: 26.0);
+const gapH28 = SizedBox(height: 28.0);
+const gapH30 = SizedBox(height: 30.0);
+const gapH32 = SizedBox(height: 32.0);
+const gapH34 = SizedBox(height: 34.0);
+const gapH36 = SizedBox(height: 36.0);
+const gapH38 = SizedBox(height: 38.0);
+const gapH40 = SizedBox(height: 40.0);
+const gapH42 = SizedBox(height: 42.0);
+const gapH44 = SizedBox(height: 44.0);
+const gapH46 = SizedBox(height: 46.0);
+const gapH48 = SizedBox(height: 48.0);
+const gapH50 = SizedBox(height: 50.0);
 
-const gaph8 = SizedBox(
-  height: 8.0,
-);
-
-const gaph12 = SizedBox(
-  height: 12.0,
-);
-const gaph16 = SizedBox(
-  height: 16.0,
-);
-const gapH24 = SizedBox(
-  height: 24.0,
-);
-
-const gapw4 = SizedBox(
-  width: 4.0,
-);
-
-const gapw8 = SizedBox(
-  width: 8.0,
-);
-
-const gapw12 = SizedBox(
-  width: 12.0,
-);
-const gapw16 = SizedBox(
-  width: 16.0,
-);
-const gapw24 = SizedBox(
-  width: 24.0,
-);
+// Horizontal Gaps
+const gapW4 = SizedBox(width: 4.0);
+const gapW8 = SizedBox(width: 8.0);
+const gapW12 = SizedBox(width: 12.0);
+const gapW16 = SizedBox(width: 16.0);
+const gapW18 = SizedBox(width: 18.0);
+const gapW20 = SizedBox(width: 20.0);
+const gapW24 = SizedBox(width: 24.0);
+const gapW26 = SizedBox(width: 26.0);
+const gapW28 = SizedBox(width: 28.0);
+const gapW30 = SizedBox(width: 30.0);
+const gapW32 = SizedBox(width: 32.0);
+const gapW34 = SizedBox(width: 34.0);
+const gapW36 = SizedBox(width: 36.0);
+const gapW38 = SizedBox(width: 38.0);
+const gapW40 = SizedBox(width: 40.0);
+const gapW42 = SizedBox(width: 42.0);
+const gapW44 = SizedBox(width: 44.0);
+const gapW46 = SizedBox(width: 46.0);
+const gapW48 = SizedBox(width: 48.0);
+const gapW50 = SizedBox(width: 50.0);
