c849341985606b0e7b0f8dee1f3bbbf7d052da57
Kwe <Kweayon@gmail.com>
2024-01-30 23:03:35 -0600

    feat: implemented sign in view model

---
+++ lib/features/authentication/ui/sign_in_view_model.dart
@@ -0,0 +1,35 @@
+import 'package:chatapp/shared/models/base_user.dart';
+import 'package:flutter/material.dart';
+import 'package:chatapp/shared/services/services.dart';
+
+class SignInViewModel extends ChangeNotifier {
+  SignInViewModel();
+
+  // TODO: use mutable variables instead of controller text
+  //The form key is used to validate the form
+  final formKey = GlobalKey<FormState>();
+
+  // Controllers
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  Future<String?> signInWithEmailAndPassword() async {
+    return await firebaseService.signInWithEmailAndPassword(emailController.text, passwordController.text);
+  }
+
+  // TODO: maybe temporally coupled with signInWithEmailAndPassword | fix it at some point
+  Future<BaseUser> createCurrentUser() async {
+    final String userid = firebaseService.currentUser!.uid;
+
+    // Currently logged in user data
+    final currentUser = await userService.getCurrentUser('users', userid);
+
+    return currentUser;
+  }
+
+  void clearControllers() {
+    emailController.clear();
+    passwordController.clear();
+    notifyListeners();
+  }
+}
