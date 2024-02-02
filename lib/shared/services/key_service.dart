54ce704b66f090231e3e6130eb4b81e0acdb9aa0
Kwe <Kweayon@gmail.com>
2024-02-02 14:55:14 -0600

    feat: added keyService

---
+++ lib/shared/services/key_service.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class KeyService {
+  final GlobalKey<ScaffoldMessengerState> scaffoldMessengerKey = GlobalKey<ScaffoldMessengerState>();
+  final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+}
