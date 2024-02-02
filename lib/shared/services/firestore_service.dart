3e9f2591f3c77db494ab453bc223e359f5cbb3f0
Kwe <Kweayon@gmail.com>
2024-02-02 14:54:40 -0600

    refact: changed the name of private methods and edited comments

--- lib/shared/services/firestore_service.dart
+++ lib/shared/services/firestore_service.dart
@@ -1,61 +1,52 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-// TODO: Refactor
-
+/// API to interact with [FirebaseFirestore] collections and documents
 class FireStoreService {
   const FireStoreService();
 
-  /// [FirebaseFirestore] instance
   static FirebaseFirestore get _firebaseFirestore => FirebaseFirestore.instance;
 
   FirebaseFirestore get instance => _firebaseFirestore;
 
-  /// [CollectionReference] to the specified Firestore collection path
-  static CollectionReference<Map<String, dynamic>> _returnCollection(
-          {required FirebaseFirestore firestore, required String collectionPath}) =>
-      firestore.collection(collectionPath);
+  /// [CollectionReference] to the specified Firestore collection path.
+  CollectionReference<Map<String, dynamic>> _getCollectionReference(
+          {required FirebaseFirestore firestoreInstance, required String collectionPath}) =>
+      firestoreInstance.collection(collectionPath);
 
-//TODO: Maybe shorten the implementation a bit an need to make descending optional
-  static Query<Map<String, dynamic>> _returnCollectionOrdered(
-          {required CollectionReference<Map<String, dynamic>> collectionRef, required String orderBy, bool descending = true}) =>
-      collectionRef.orderBy(orderBy, descending: descending);
+  /// Returns a [QuerySnapshot] containing the results of a query, which can contain zero or more [DocumentSnapshot] objects.
+  Stream<QuerySnapshot<Map<String, dynamic>>> _getCollectionSnapshots({
+    required CollectionReference<Map<String, dynamic>> collectionRef,
+    String? orderBy,
+    bool descending = true,
+  }) =>
+      orderBy == null ? collectionRef.snapshots() : collectionRef.orderBy(orderBy, descending: descending).snapshots();
 
-  /// Takes a [CollectionReference] to the currently configured [FirebaseFirestore]
-  /// Returns a [QuerySnapshot]
-  /// The returned [QuerySnapshot] contains the results of a query
-  /// It can contain zero or more [DocumentSnapshot] objects
+  /// Returns a [List] of all [QueryDocumentSnapshot] objects containing data read from a document
+  /// in your [FirebaseFirestore] database as part of a query.
+  Stream<List<QueryDocumentSnapshot<Map<String, dynamic>>>> _getQueryDocumentSnapshots(
+          {required Stream<QuerySnapshot<Map<String, dynamic>>> snapshots}) =>
+      snapshots.map((snapshot) => snapshot.docs);
 
-  static Stream<QuerySnapshot<Map<String, dynamic>>> _returnCollectionSnapshot(
-          {required CollectionReference<Map<String, dynamic>> collectionRef, String? orderBy}) =>
-      orderBy == null ? collectionRef.snapshots() : _returnCollectionOrdered(collectionRef: collectionRef, orderBy: orderBy).snapshots();
-
-  /// Takes a [QuerySnapshot] as an argument
-  /// Returns a [List] of all [QueryDocumentSnapshot] objects
-  /// A [QueryDocumentSnapshot] object
-  /// contains data read from a document in your [FirebaseFirestore]
-  /// database as part of a query
-
-  static Stream<List<QueryDocumentSnapshot<Map<String, dynamic>>>> _returnListDocsSnapshot(
-          {required Stream<QuerySnapshot<Map<String, dynamic>>> snapshot}) =>
-      snapshot.map((snapshot) => snapshot.docs);
-
-  /// Takes a [List] of [QueryDocumentSnapshot] objects as a argument
-  /// Returns a [List] of each [QueryDocumentSnapshot] objects data as a [Map]
-
-  static Stream<List<Map<String, dynamic>>> _returnListDocsData(
+  /// Decodes a [List] of [QueryDocumentSnapshot] objects into a [List] of [Map] objects.
+  Stream<List<Map<String, dynamic>>> _decodeQueryDocumentSnapshots(
           {required Stream<List<QueryDocumentSnapshot<Map<String, dynamic>>>> documentSnapshots}) =>
       documentSnapshots.map((listDocSnapshot) => listDocSnapshot.map((doc) => doc.data()).toList());
 
-  /// Returns all documents of a given collection as a [List] of [Map]'s
-  Stream<List<Map<String, dynamic>>> getListDocsData({required String collectionPath, String? orderBy}) =>
-      FireStoreService._returnListDocsData(
-        documentSnapshots: _returnListDocsSnapshot(
-          snapshot: _returnCollectionSnapshot(
-            collectionRef: _returnCollection(
-              firestore: FireStoreService._firebaseFirestore,
+  /// Returns all documents of a given collection as a [List] of [Map] objects.
+  Stream<List<Map<String, dynamic>>> getAllDocuments({
+    required String collectionPath,
+    String? orderBy,
+    bool descending = true,
+  }) =>
+      _decodeQueryDocumentSnapshots(
+        documentSnapshots: _getQueryDocumentSnapshots(
+          snapshots: _getCollectionSnapshots(
+            collectionRef: _getCollectionReference(
+              firestoreInstance: _firebaseFirestore,
               collectionPath: collectionPath,
             ),
             orderBy: orderBy,
+            descending: descending,
           ),
         ),
       );
