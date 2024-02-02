7a98ce96211468ef3ddfd42c53a2a29a2abf4071
Kwe <Kweayon@gmail.com>
2024-02-02 14:56:27 -0600

    feat: context is now optional to show snackbar

--- lib/shared/services/toast_service.dart
+++ lib/shared/services/toast_service.dart
@@ -1,28 +1,36 @@
+import 'package:chatapp/shared/services/services.dart';
 import 'package:flutter/material.dart';
 
 class ToastService {
-  void showSnackBar(BuildContext context, String message) {
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(
-        behavior: SnackBarBehavior.floating,
-        backgroundColor: Colors.white,
-        padding: const EdgeInsets.symmetric(
-          vertical: 20.0,
-          horizontal: 12.0,
-        ),
-        content: Center(
-          child: FittedBox(
-            child: Text(
-              message,
-              style: const TextStyle(
-                color: Colors.red,
-                fontSize: 16,
-                fontWeight: FontWeight.w600,
-              ),
+  void showSnackBar(String message, [BuildContext? context]) {
+    final snackBar = _getSnackBar(message);
+    context == null
+        ? keyService.scaffoldMessengerKey.currentState!.showSnackBar(
+            snackBar,
+          )
+        : ScaffoldMessenger.of(context).showSnackBar(
+            snackBar,
+          );
+  }
+}
+
+SnackBar _getSnackBar(String message) => SnackBar(
+      behavior: SnackBarBehavior.floating,
+      backgroundColor: Colors.white,
+      padding: const EdgeInsets.symmetric(
+        vertical: 20.0,
+        horizontal: 12.0,
+      ),
+      content: Center(
+        child: FittedBox(
+          child: Text(
+            message,
+            style: const TextStyle(
+              color: Colors.red,
+              fontSize: 16,
+              fontWeight: FontWeight.w600,
             ),
           ),
         ),
       ),
     );
-  }
-}
