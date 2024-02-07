21494dc1a91fee0c2a3ea13c874fe45f7fe26ba2
Kwe <Kweayon@gmail.com>
2024-02-07 17:56:27 -0600

    feat: tested login flow

--- test/features/authentication/ui/sign_in_view_model_test.dart
+++ test/features/authentication/ui/sign_in_view_model_test.dart
@@ -1,5 +1,11 @@
+import 'package:chatapp/app/general/constants.dart';
 import 'package:chatapp/features/authentication/ui/sign_in_view_model.dart';
+import 'package:chatapp/shared/services/services.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+import '../../../setup/test_data.dart';
+import '../../../setup/test_helpers.dart';
 
 void main() {
   group("SignInViewModel", () {
@@ -31,6 +37,33 @@ void main() {
       expect(model.password, password);
 
       expect(model.switchState, isSwitchedOn);
+    });
+
+    test("when email and password filled with registered user, then user signin is successful", () async {
+      // Arrange = Setup
+
+      final model = getModel();
+
+      getAndRegisterFirebaseService(
+        email: testUser.email,
+        password: testUser.password,
+      );
+
+      getAndRegisterUserService();
+
+      // Act
+
+      model.setEmail(testUser.email);
+
+      model.setPassword(testUser.password);
+
+      await model.signInWithEmailAndPassword();
+
+      // Assert - Result
+
+      verify(() => firebaseService.signInWithEmailAndPassword(testUser.email, testUser.password)).called(1);
+      verify(() => userService.getCurrentUser(CollectionPath.users.path, testUser.id)).called(1);
+      verify(() => firebaseService.currentUser).called(1);
     });
   });
 }
