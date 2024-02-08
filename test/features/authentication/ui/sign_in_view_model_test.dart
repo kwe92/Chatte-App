9a4a2db4878e2d67d2973d08a639a3cdc9215056
Kwe <Kweayon@gmail.com>
2024-02-07 18:12:08 -0600

    feat: 100% method test coverage on SignInViewModel

--- test/features/authentication/ui/sign_in_view_model_test.dart
+++ test/features/authentication/ui/sign_in_view_model_test.dart
@@ -39,10 +39,48 @@ void main() {
       expect(model.switchState, isSwitchedOn);
     });
 
-    test("when email and password filled with registered user, then user signin is successful", () async {
-      // Arrange = Setup
+    test('when clearData called, then all data cleared', () {
+      // Arange - Setup
 
       final model = getModel();
+
+      // Act
+
+      model.setEmail(testUser.email);
+
+      model.setPassword(testUser.password);
+
+      model.emailController.text = testUser.email;
+
+      model.passwordController.text = testUser.password;
+
+      // Assert - Result
+
+      expect(model.email, testUser.email);
+
+      expect(model.emailController.text, testUser.email);
+
+      expect(model.password, testUser.password);
+
+      expect(model.passwordController.text, testUser.password);
+
+      // Act
+
+      model.clearData();
+
+      // Assert - Result
+
+      expect(model.email, '');
+
+      expect(model.emailController.text, '');
+
+      expect(model.password, '');
+
+      expect(model.passwordController.text, '');
+    });
+
+    test('when createCurrentUser called, then current user is retrieved', () async {
+      // Arrange - Setup
 
       getAndRegisterFirebaseService(
         email: testUser.email,
@@ -50,6 +88,32 @@ void main() {
       );
 
       getAndRegisterUserService();
+
+      final model = getModel();
+
+      // Act
+
+      final currentUser = await model.createCurrentUser();
+
+      // Assert - Result
+
+      verify(() => userService.getCurrentUser(CollectionPath.users.path, testUser.id)).called(1);
+      verify(() => firebaseService.currentUser).called(1);
+
+      expect(currentUser, testUser);
+    });
+
+    test("when email and password filled with registered user, then user signin is successful", () async {
+      // Arrange = Setup
+
+      getAndRegisterFirebaseService(
+        email: testUser.email,
+        password: testUser.password,
+      );
+
+      getAndRegisterUserService();
+
+      final model = getModel();
 
       // Act
 
