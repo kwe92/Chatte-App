144fc39c3c0b2c31450e01bcc3740b92b42b2bf5
Kwe <Kweayon@gmail.com>
2024-02-09 11:11:27 -0600

    feat: implemented more mocks

--- test/setup/test_mocks.dart
+++ test/setup/test_mocks.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: subtype_of_sealed_class
 
+import 'package:chatapp/shared/services/chat_service.dart';
 import 'package:chatapp/shared/services/firebase_service.dart';
 import 'package:chatapp/shared/services/image_picker_service.dart';
 import 'package:chatapp/shared/services/toast_service.dart';
@@ -24,6 +25,8 @@ class MockImagePickerPlatform extends Mock implements ImagePickerPlatform {}
 class MockToastService extends Mock implements ToastService {}
 
 class MockCollectionReference<T> extends Mock implements CollectionReference {}
+
+class MockChatService extends Mock implements ChatService {}
 
 class MockFirebaseUser extends Mock implements User {
   @override
